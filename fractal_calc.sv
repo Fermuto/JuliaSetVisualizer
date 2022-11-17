b399b549b6e3d4626ebb213af63c239fa3a1dfa6
Alexander Jin <amjin2@illinois.edu>
2022-11-16 21:16:51 -0600

    push for u

--- fractal_calc.sv
+++ fractal_calc.sv
@@ -1,10 +1,27 @@
+'define img_width 640
+'define img_height 480
 module fractal_calc(
 
 
 );
 begin
 
-real real_var, cpx_var;
-string color_var;
+	real real_var, cpx_var;
+	string color_var;
 
+
+
+	get_color rgbgen();
+
+	always_comb
+	begin
+		for ()
+		begin
+			for ()
+			begin
+				
+			end
+		end
+	end
+	
 endmodule
